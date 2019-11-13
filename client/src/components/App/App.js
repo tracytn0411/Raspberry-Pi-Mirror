d26e26a4311bc40eaa4c582528048de88251b367
TracyTN <tracytn0411@gmail.com>
2019-11-12 19:58:41 -0800

    Smart mirror layout.

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import {Container, Row} from 'react-bootstrap';
-import Moment from './Moment'
+import './App.css';
+import {Container, Row, Col} from 'react-bootstrap';
+import Moment from './Moment';
+import Weather from './Weather';
 
 function App() {
   return (
     <Container fluid className="App">
-      <Row>
-        <Moment />
+      <Row className='Moment'>
+        <Col>
+          <Moment />
+        </Col>
+        <Col className='Weather text-right'>
+          <Weather />
+        </Col>
+      </Row>
+      <Row className='Calendar'>
+        <Col>
+          <p>Calendar API goes here</p>
+        </Col>
+        <Col className='Forecase text-right'>
+          <p>Weather Forecast goes here</p>
+        </Col>
+      </Row>
+      <Row className='News mt-auto'>
+        <Col>
+          <p>News Feed goes here</p>
+        </Col>
+        <Col className='Geo text-right'>
+          <p>Google Geo API goes here</p>
+        </Col>
       </Row>
     </Container>
   );
---
+++ client/src/components/App/Weather.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+
+class Weather extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+
+  render() {
+    return (
+      <>
+        <p>Weather API goes here</p>
+      </>
+    )
+  }
+}
+
+export default Weather;
