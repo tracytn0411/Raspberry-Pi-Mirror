023d43430534afba8e5fc778c7a2f4862f2714c6
Tracy Nguyen <46982260+tracytn0411@users.noreply.github.com>
2019-12-05 13:49:28 -0800

    App Authentication (#10)
    * Add login page.
    
    * Authentication utilizes private routes and local storage.

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,20 +1,29 @@
-import React from "react";
-
-// import "./App.css";
-// import { Container, Row, Col } from "react-bootstrap";
-// import Moment from "../components/Moment";
-// import Weather from "../components/Weather";
-// import News from "../components/News";
-// import Forecast from "../components/Forecast";
-// import Map from "../components/Map";
-// import Commute from "../components/Commute"
-
-import Router from './Router'
-
-function App() {
-  return (
-    <Router />
-  )
+import React, {Component} from "react";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
+import Display from "./Display";
+import HomePage from "./HomePage";
+import Secret from '../auth/Secret'
+import verifyToken from '../auth/verifyToken'
+import LoginPage from '../auth/LoginPage'
+import RegisterPage from '../auth/RegisterPage'
+import PrivateRoute from '../auth/PrivateRoute'
+class App extends Component {
+  
+  render(){
+    return(
+      
+    <BrowserRouter>
+    <Switch>
+      {/* <Route exact path="/" component={LoginPage} /> */}
+      <Route path="/login" component={LoginPage} />
+      <Route path="/secret" component={verifyToken(Secret)} />
+      <Route path='/register' component={RegisterPage} />
+      <PrivateRoute exact path='/' component={HomePage} />
+      <PrivateRoute exact path='/display' component={Display} />
+    </Switch>
+  </BrowserRouter>
+    )
+  }
 }
 
 export default App;
